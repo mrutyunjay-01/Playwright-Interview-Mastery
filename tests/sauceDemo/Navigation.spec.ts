adc240471c47e12d413b24a6c89ad98769e481d7
likan01 <mrutyunjaykar2001@gmail.com>
2026-08-07 00:01:14 +0530

    feat(tests): add new page object model files and update navigation test for product interaction

--- tests/sauceDemo/Navigation.spec.ts
+++ tests/sauceDemo/Navigation.spec.ts
@@ -1,18 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('nav-001', async({ page }) =>{
-    await page.goto('https://www.saucedemo.com/inventory.html');
 
-    await test.step('scroll', async() => {
-        let page_footer = page.locator('[data-test="footer"]');
-        expect(page.locator('[class="bm-menu-wrap"]')).toHaveAttribute('aria-hidden','true');
-        await page.locator('#react-burger-menu-btn').click();
-        expect(page.locator('[class="bm-menu-wrap"]')).toHaveAttribute('aria-hidden','false');
-        expect(page.locator('[data-test="inventory-sidebar-link"]')).toBeVisible;
-    })
-});
-
-test('nav-002 Click on a product, Add to cart and Verify', async({ page }) => {
+test('nav-001 Click on a product, Add to cart and Verify', async({ page }) => {
 
     page.goto('https://www.saucedemo.com/inventory.html');
 
